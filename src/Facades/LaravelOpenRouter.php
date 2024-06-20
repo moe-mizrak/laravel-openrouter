9ff02852d8469b6c6d93f4b529243b48fc3a3e27
Moe Mizrak <mhmmdmzrk@gmail.com>
2024-06-20 17:10:33 +0300

    stream chat request logic and maintaining conversation continuity are added (#14)
    * stream chat completion feature is fixed
    
    * return raw response from chatStreamRequest and add Maintaining Conversation Continuity section
    
    * typo fixed

--- src/Facades/LaravelOpenRouter.php
+++ src/Facades/LaravelOpenRouter.php
@@ -2,16 +2,20 @@
 
 namespace MoeMizrak\LaravelOpenrouter\Facades;
 
+use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Support\Facades\Facade;
 use MoeMizrak\LaravelOpenrouter\DTO\ChatData;
 use MoeMizrak\LaravelOpenrouter\DTO\CostResponseData;
+use MoeMizrak\LaravelOpenrouter\DTO\ErrorData;
 use MoeMizrak\LaravelOpenrouter\DTO\LimitResponseData;
 use MoeMizrak\LaravelOpenrouter\DTO\ResponseData;
 
 /**
  * Facade for LaravelOpenRouter.
  *
- * @method static ResponseData chatRequest(ChatData $chatData) Sends a chat request to the OpenRouter API and returns the response data.
+ * @method static ErrorData|ResponseData chatRequest(ChatData $chatData) Sends a chat request to the OpenRouter API and returns the response data.
+ * @method static PromiseInterface chatStreamRequest(ChatData $chatData) Sends a chat stream request to the OpenRouter API and returns the raw streaming response.
+ * @method static array filterStreamingResponse(string $streamingResponse) It filters streaming response string so that response string is mapped into ResponseData.
  * @method static CostResponseData costRequest(string $generationId) Sends a cost request to the OpenRouter API with the given generation ID and returns the cost response data.
  * @method static LimitResponseData limitRequest() Sends a limit request to the OpenRouter API and returns the limit response data.
  */
