87c6f3754c90a123fe579552713850bcbf79e266
moe-mizrak <mhmmdmzrk@gmail.com>
2025-06-22 18:18:44 +0300

    fix: move helper methods to a separate helper class

--- src/OpenRouterAPI.php
+++ src/OpenRouterAPI.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace MoeMizrak\LaravelOpenrouter;
 
+use MoeMizrak\LaravelOpenrouter\Helpers\OpenRouterHelper;
+
 /**
  * This abstract class forms the response from OpenRouter
  *
  * Class OpenRouterAPI
+ *
  * @package MoeMizrak\LaravelOpenrouter
  */
 abstract class OpenRouterAPI
 {
+    /**
+     * RekognitionAPI constructor.
+     *
+     * @param OpenRouterHelper $openRouterHelper
+     */
+    public function __construct(
+        protected OpenRouterHelper $openRouterHelper,
+    ) {}
 }
